f1da70e6967d88048fed8cba25291eec54194a8b
Max Wofford <max.speed.wofford@gmail.com>
2015-02-13 21:50:36 -0800

    fix for waiting for community page to load

--- app.rb
+++ app.rb
@@ -5,14 +5,6 @@ def is_flag_on?(flag)
   ARGV.include?(flag)
 end
 
-def sign_in(driver)
-  puts 'signing in'
-  driver.find_element(:id, 'popup-email').send_keys(R_USERNAME)
-  driver.find_element(:id, 'popup-pwd').send_keys(R_PASSWORD)
-  driver.find_element(:id, 'popup-pwd').submit
-  puts 'signed in'
-end
-
 if is_flag_on?("--firefox")
   driver = Selenium::WebDriver.for :firefox
 else
@@ -21,7 +13,13 @@ else
 end
 
 driver.navigate.to 'http://raptr.com/login'
-sign_in(driver)
+puts 'signing in'
+driver.find_element(:id, 'popup-email').send_keys(R_USERNAME)
+driver.find_element(:id, 'popup-pwd').send_keys(R_PASSWORD)
+driver.find_element(:id, 'popup-pwd').submit
+puts 'signed in'
+sleep(1)
+puts 'heading to community page'
 driver.navigate.to 'http://raptr.com/community'
 IO.binwrite("bot-log.txt","#{driver.title}:#{Time.now}")
 driver.quit
